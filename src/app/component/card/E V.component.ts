d706f2e14a9524bc799aa7019f82eed67487d120
emptymind1251998 <y.rahulnikki1998@gmail.com>
2018-11-27 16:53:29 +0530

    Add files via upload
    nikki

---
+++ src/app/component/card/E V.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  templateUrl: 'E V.component.html'
+})
+export class CardsComponent {}
+
---
+++ src/app/component/card/S S.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  templateUrl: 'S S.html'
+})
+export class CardsComponent {}
+
